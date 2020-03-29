dcbf81982cc170665bce6633e0995c8840f898dc
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-29 12:36:24 +0200

    3.2: added info page with time stamp

--- index.js
+++ index.js
@@ -34,50 +34,12 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
-// app.get('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const note = persons.find(note => note.id === id)
-//     if (note) {
-//         response.json(note)
-//     } else {
-//         response.status(404).end()
-//     }
-// })
-
-// app.delete('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     persons = persons.filter(note => note.id !== id)
-
-//     response.status(204).end()
-// })
-
-// const generateId = () => {
-//     const maxId = persons.length > 0
-//         ? Math.max(...persons.map(n => n.id))
-//         : 0
-//     return maxId + 1
-// }
-
-// app.post('/api/notes', (request, response) => {
-//     const body = request.body
-
-//     if (!body.content) {
-//         return response.status(400).json({
-//             error: 'content missing'
-//         })
-//     }
-
-//     const note = {
-//         content: body.content,
-//         important: body.important || false,
-//         date: new Date(),
-//         id: generateId(),
-//     }
-
-//     persons = persons.concat(note)
-
-//     response.json(note)
-// })
+app.get('/info', (req, res) => {
+    res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {

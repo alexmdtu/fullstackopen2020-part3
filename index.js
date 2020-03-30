52b7e492b0a8fa9b36ef47811135dc77cfbf5c30
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-30 14:14:36 +0200

    3.7: added morgan middleware

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 let persons = [
@@ -25,6 +27,8 @@ let persons = [
 ]
 
 app.use(express.json())
+
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')

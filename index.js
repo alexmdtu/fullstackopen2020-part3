8402aee7113e74f28a27f309ab676c3ab5a9ea5d
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-30 16:05:59 +0200

    heroku prep work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -27,6 +28,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('person', (request, response) => {
     return JSON.stringify(request.body)
@@ -108,7 +110,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

0110759296ef42647aadc8be58fb1e1d461f8129
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-30 16:13:52 +0200

    uibuild

---
+++ build/precache-manifest.f5024c084082d1e60d43885271526511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b04d2e7d569d67b887a1ad2bd04e9a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35658a72390815255e11",
+    "url": "/static/js/2.66c1b036.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66c1b036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ece7568cb773c061020",
+    "url": "/static/js/main.ca394dd5.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);

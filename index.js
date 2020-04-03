efc012b9afd1191a934047a32d424ed151274713
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-03 10:15:27 +0200

    3.15: added deletion functionality to backend

--- index.js
+++ index.js
@@ -71,11 +71,12 @@ app.get('/api/persons/:id', (request, response) => {
         })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const getRandomInt = () => {
@@ -111,6 +112,17 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson.toJSON())
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

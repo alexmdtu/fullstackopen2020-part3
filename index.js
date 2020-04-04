fb3cd9b747911777ac38fc8ca57cc7ef66fe4461
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-04 14:09:21 +0200

    3.22: added ESLint and fixed errors

--- index.js
+++ index.js
@@ -5,39 +5,16 @@ const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "01487621",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
-]
-
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-morgan.token('person', (request, response) => {
+morgan.token('person', (request) => {
     return JSON.stringify(request.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person', {
-    skip: (req, res) => res.statusCode != 200
+    skip: (req, res) => res.statusCode !== 200
 }))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {
@@ -62,10 +39,7 @@ app.get('/info', (req, res) => {
             <p>${new Date()}</p>
             `)
         })
-        .catch(error => {
-            response.status(404).end()
-        })
-
+        .catch(res.status(404).end())
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -82,15 +56,9 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
+        .then(response.status(204).end())
         .catch(error => next(error))
 })
-
-const getRandomInt = () => {
-    return Math.floor(Math.random() * 50000)
-}
 
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
@@ -135,7 +103,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 const errorHandler = (error, request, response, next) => {
-    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })

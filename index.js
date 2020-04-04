5f9ef1a1369731d57702c6a3134c2bd7556c6bde
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-04 13:01:24 +0200

    3.19: added validation for unique name entries

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ const getRandomInt = () => {
     return Math.floor(Math.random() * 50000)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (body.name === undefined) {
@@ -112,9 +112,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson.toJSON())
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -137,6 +139,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
